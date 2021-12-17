327d3aaf70d47107459c7b7680263c130825d877
Adam Khalguev <halgadam@gmail.com>
2021-12-17 13:56:29 +0300

    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import React, {useEffect, useState} from 'react';
-import TokenMintForm from "./components/TokenMintForm";
-import Web3 from 'web3'
-import {Config} from "./components/Config";
+import React, { useEffect, useState } from 'react';
+import TokenMintForm from './components/TokenMintForm';
+import Web3 from 'web3';
+import { Config } from './components/Config';
+import './App.css';
 
-export default function App () {
-    const [account, setAccount] = useState('');
-    const [contract, setContract] = useState<any>();
+export default function App() {
+  const [account, setAccount] = useState('');
+  const [contract, setContract] = useState<any>();
 
-    async function connect() {
-        if (+Web3.givenProvider.chainId !== Config.chainId) {
-            alert('Not bsc, please change chain id');
-            return
-        }
-
-        const web3 = new Web3(Web3.givenProvider);
-        const accounts = await web3.eth.requestAccounts();
-
-        setContract(new web3.eth.Contract(JSON.parse(Config.contractAbi), Config.contractAddress));
-
-        setAccount(accounts[0]);
+  async function connect() {
+    if (+Web3.givenProvider.chainId !== Config.chainId) {
+      alert('Not bsc, please change chain id');
+      return;
     }
 
+    const web3 = new Web3(Web3.givenProvider);
+    const accounts = await web3.eth.requestAccounts();
 
-    return (
-        <div>
-            { account === '' ?
-                <button onClick={connect}>connectWallet</button>
-                : <TokenMintForm contract={contract} address={account}/>
-            }
-        </div>
+    setContract(
+      new web3.eth.Contract(
+        JSON.parse(Config.contractAbi),
+        Config.contractAddress
+      )
     );
+
+    setAccount(accounts[0]);
+  }
+
+  return (
+    <div className="main">
+      {account === '' ? (
+        <button className="main__btn" onClick={connect}>
+          Connect Wallet
+        </button>
+      ) : (
+        <TokenMintForm contract={contract} address={account} />
+      )}
+    </div>
+  );
 }
--- src/components/TokenMintForm.tsx
+++ src/components/TokenMintForm.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
 
-export default function(props: any) {
-    const { contract, address } = props;
+export default function (props: any) {
+  const { contract, address } = props;
 
-    const [
-        tokenStakedAmount,
-        setTokenStakedAmount
-    ] = React.useState(0);
-    const tokenAmountRef = React.useRef<any>(tokenStakedAmount);
+  const [tokenStakedAmount, setTokenStakedAmount] = React.useState(0);
+  const tokenAmountRef = React.useRef<any>(tokenStakedAmount);
 
-    const changeTokenAmount = (event: React.ChangeEvent<HTMLInputElement>) => setTokenStakedAmount(
-        +event.currentTarget.value.replace(/[^0-9]+/g, "")
-    );
+  const changeTokenAmount = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setTokenStakedAmount(+event.currentTarget.value.replace(/[^0-9]+/g, ''));
 
+  const mint = async () => {
+    const cost = await contract.methods.cost().call();
+    contract.methods
+      .mint(tokenStakedAmount)
+      .send({ from: address, value: cost * tokenStakedAmount })
+      .then(console.log);
+  };
 
-    const mint = async () => {
-        const cost = await contract.methods.cost().call()
-        contract.methods.mint(tokenStakedAmount).send({from: address, value: cost * tokenStakedAmount}).then(console.log)
-    }
+  return (
+    <>
+      <div className="form">
+        <input
+          id="inp_text"
+          className="main__input"
+          type="text"
+          autoComplete="off"
+          required
+          ref={tokenAmountRef}
+          onChange={changeTokenAmount}
+        />
+        <label htmlFor="#inp_text" className="label_name">
+          <span className="content_name">ENTER A NUMBER</span>
+        </label>
+      </div>
 
-    return (
-        <>
-            <input type="text" ref={tokenAmountRef} onChange={changeTokenAmount} />
-            <button onClick={mint}>Mint</button>
-        </>
-    );
+      <button className="main__btn_mint" onClick={mint}>
+        Mint
+      </button>
+    </>
+  );
 }
-
-
